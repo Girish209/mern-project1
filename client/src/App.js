fa9bd74c122ec9052822b0d06c62251970f72fb2
M Girish Rao <121603439+Girish209@users.noreply.github.com>
2023-07-11 06:23:52 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Todoform from './components/Todoform';
 import Todos from './components/Todos';
+import './media.css'
 function App() {
   return (
     <div>
